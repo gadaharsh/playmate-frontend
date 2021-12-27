28e0c2261c6135c0e6553878f0a33d95a09c6505
gadaharsh <gada.hp@somaiya.edu>
2021-12-27 14:53:07 +0530

    charts created

--- src/Components/Charts/PieChart.js
+++ src/Components/Charts/PieChart.js
@@ -9,8 +9,22 @@ const PieChart = (props) => {
     datasets: [
       {
         data: props.value,
-        backgroundColor: props.colors[0],
-        borderColor: props.colors[1],
+        backgroundColor: [
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          `rgba(255, 99, 132, 0.2)`,
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(75, 192, 192, 1)",
+          "rgba(54, 162, 235, 1)",
+          `rgba(255, 99, 132, 1)`,
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
         borderWidth: 1,
       },
     ],
